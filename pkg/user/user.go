d057560233403ca06897770b530bd9778012378e
Vitor Silva <vitor.azevedo009@gmail.com>
2022-12-23 11:03:00 -0300

    Addinf FetchUser function

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -1,10 +1,42 @@
 package user
 
 import (
+	"errors"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 )
 
-func FetchUser(email string, tablename string, dynaClient dynamodbiface.DynamoDBAPI) {}
+var ErrorFailedToFetchRecord = "failed to fetch record"
+
+type User struct {
+	Email     string `json:"email"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"firstname"`
+}
+
+func FetchUser(email string, tablename string, dynaClient dynamodbiface.DynamoDBAPI) (*User, error) {
+	input := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"email": {
+				S: aws.String(email),
+			},
+		},
+		TableName: aws.String(tablename),
+	}
+	result, err := dynaClient.GetItem(input)
+	if err != nil {
+		return nil, errors.New(ErrorFailedToFetchRecord)
+	}
+	item := new(User)
+	err = dynamodbattribute.UnmarshalMap(result.Item, item)
+	if err != nil {
+		return nil, errors.New(ErrorFailedToFetchRecord)
+	}
+	return item, nil
+}
 
 func FetchUsers(tablename string, dynaClient dynamodbiface.DynamoDBAPI) {}
 
